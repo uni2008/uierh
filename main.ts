4a936f832010a5950856f8dcf4e5bef3cab82e1c
uni2008 <93262463+uni2008@users.noreply.github.com>
2021-11-10 14:20:00 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,27 @@
+basic.forever(function () {
+    if (input.isGesture(Gesture.Shake)) {
+        led.plot(2, 2)
+    } else {
+        led.unplot(2, 2)
+    }
+    if (input.isGesture(Gesture.TiltLeft)) {
+        led.plot(0, 2)
+    } else {
+        led.unplot(0, 2)
+    }
+    if (input.isGesture(Gesture.TiltRight)) {
+        led.plot(4, 2)
+    } else {
+        led.unplot(4, 2)
+    }
+    if (input.isGesture(Gesture.LogoUp)) {
+        led.plot(2, 4)
+    } else {
+        led.unplot(2, 4)
+    }
+    if (input.isGesture(Gesture.LogoDown)) {
+        led.plot(2, 0)
+    } else {
+        led.unplot(2, 0)
+    }
+})
